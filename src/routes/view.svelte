22acdcde15459f3f01d75e730f989d7450816396
Jon Ellwood <jonellwood@gmail.com>
2022-07-11 13:18:08 -0400

    fixed the rest of the icons now

--- src/routes/view.svelte
+++ src/routes/view.svelte
@@ -1,5 +1,10 @@
 <script>
 	import Hr from '../lib/hr.svelte';
+	import dev from '$lib/assets/dev-site-icon.svg';
+	import github from '$lib/assets/github-icon.png';
+	import contact from '$lib/assets/contact-icon.png';
+	import projects from '$lib/assets/projects-icon.jpg';
+	import twitter from '$lib/assets/twitter-icon.png';
 </script>
 
 <div class="white-box-in-middle-to-look-like-piece-of-paper">
@@ -18,7 +23,7 @@
 			<tr>
 				<td
 					><a href="https://jonellwood.dev" target="_blank">
-						<img src="src/public/dev-site-icon.svg" width="48px" height="48px" alt="dev icon" />
+						<img src={dev} width="48px" height="48px" alt="dev icon" />
 					</a></td
 				>
 				<td><a href="https://jonellwood.dev" target="_blank">My Dev Site</a></td>
@@ -26,7 +31,7 @@
 			<tr>
 				<td
 					><a href="https://github.com/jonellwood" target="_blank">
-						<img src="src/public/github-icon.png" width="48px" height="48px" alt="github icon" />
+						<img src={github} width="48px" height="48px" alt="github icon" />
 					</a></td
 				>
 				<td><a href="https://github.com/jonellwood" target="_blank">My Repo</a></td>
@@ -34,7 +39,7 @@
 			<tr>
 				<td
 					><a href="https://contact.jonellwood.dev" target="_blank">
-						<img src="src/public/contact-icon.png" width="48px" height="48px" alt="contact icon" />
+						<img src={contact} width="48px" height="48px" alt="contact icon" />
 					</a></td
 				>
 				<td><a href="https://contact.jonellwood.dev" target="_blank">Contact Me</a></td>
@@ -42,7 +47,7 @@
 			<tr>
 				<td
 					><a href="https://projects.jonellwood.dev" target="_blank">
-						<img src="src/public/projects-icon.jpg" width="48px" height="48px" alt="contact icon" />
+						<img src={projects} width="48px" height="48px" alt="projects icon" />
 					</a></td
 				>
 				<td><a href="https://projects.jonellwood.dev" target="_blank">Projects</a></td>
@@ -50,7 +55,7 @@
 			<tr>
 				<td
 					><a href="https://twitter.com/thejonellwood" target="_blank">
-						<img src="src/public/twitter-icon.png" width="48px" height="48px" alt="contact icon" />
+						<img src={twitter} width="48px" height="48px" alt="twitter icon" />
 					</a></td
 				>
 				<td><a href="https://twitter.com/thejonellwood" target="_blank">Twitter</a></td>
